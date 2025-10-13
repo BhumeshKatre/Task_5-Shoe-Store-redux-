0ccba675cd2a56e1cfc49bb437565141fbfc2ff4
DevDynamoWeb <dev.bhumesh@gmail.com>
2025-10-13 07:55:04 +0530

    product must be delete if quantity goes below 1 at cart

--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -13,25 +13,38 @@ const cartSlice = createSlice({
       state.cartItems.push(action.payload);
       state.itemTotalPrice += action.payload.price;
     },
-    removeFromCart: () => {},
+    removeFromCart: () => {
+      const filterCart = state.cartItems.filter((items) => items !== item);
+      // console.log(filterCart);
+      state.cartItems = filterCart;
+    },
 
     increaseItem: (state, action) => {
       const item = state.cartItems.find((i) => i.id === action.payload);
+
       if (item) {
         item.quantity += 1;
       }
-      state.itemTotalPrice = state.cartItems.reduce((sum, i) => 
-        sum + i.price * i.quantity,0
+
+      state.itemTotalPrice = state.cartItems.reduce(
+        (sum, i) => sum + i.price * i.quantity,
+        0
       );
     },
 
     decreaseItem: (state, action) => {
       const item = state.cartItems.find((i) => i.id === action.payload);
+      if (item.quantity === 1) {
+        const filterCart = state.cartItems.filter((items) => items !== item);
+        // console.log(filterCart);
+        state.cartItems = filterCart;
+      }
       if (item && item.quantity > 1) {
         item.quantity -= 1;
       }
-      state.itemTotalPrice = state.cartItems.reduce((sum, i) => 
-        sum + i.price * item.quantity, 0
+      state.itemTotalPrice = state.cartItems.reduce(
+        (sum, i) => sum + i.price * item.quantity,
+        0
       );
     },
   },
